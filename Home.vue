354d08cfabe8dc4a86acd098c50276221b007342
Shohei Ito <skytree.skysky@gmail.com>
2022-05-03 15:01:15 +0900

    2

---
+++ Home.vue
@@ -0,0 +1,16 @@
+export default {
+  computed: {
+    username(){
+      return this.$route.params.username
+    },
+  },
+  methods: {
+    goToDashbord(){
+      if(isAuthenticated){
+        this.$router.path('/dashbord')
+      }else{
+        this.$router.push('/login')
+      }
+    },
+  },
+}
